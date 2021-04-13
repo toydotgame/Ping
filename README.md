52d4fa895c778af986f571a367c499002b6ca41b
toydotgame <toydotgame@gmail.com>
2021-04-13 18:19:50 +1000

    Added base plugin files Copied from toydotgame/SleepMessage. plugin.yml from toydotgame/Thisway.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ping
+An optimised plugin to get someone's ping, whilst using minimal server resources.
---
+++ src/io/github/toydotgame/Ping/Main.java
@@ -0,0 +1,10 @@
+package io.github.toydotgame.Ping;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {	
+	@Override
+	public void onEnable() {
+		System.out.print("[Ping] Plugin loaded successfully!");
+	}
+}
